1a136d04af88a21e81cfc184081dde493f7f052f
Cory Jennings <corydjennings@gmail.com>
2026-09-11 12:34:29 -0400

    update for switching to other computer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import Routes from "./Routes";
 import { ExpensesDemoContextProvider } from "./context";
 import { AuthContextProvider } from "./context/AuthContext";
+import ScrollToTop from "./components/ScrollToTop";
+
 
 function App() {
   return (
     <AuthContextProvider>
       <ExpensesDemoContextProvider>
+        {/* <ScrollToTop /> */}
         <Routes />
       </ExpensesDemoContextProvider>
     </AuthContextProvider>
